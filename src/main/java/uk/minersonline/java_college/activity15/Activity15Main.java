ceb7d841ef01d30f6861364b7b8dde8c3a50c3bb
ajh123 <samrosoft36@gmail.com>
2024-01-31 14:23:46 +0000

    refactor: improve logic

--- src/main/java/uk/minersonline/java_college/activity15/Activity15Main.java
+++ src/main/java/uk/minersonline/java_college/activity15/Activity15Main.java
@@ -38,36 +38,36 @@ public class Activity15Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Player[] players = new Player[2];
 
-        System.out.println("Enter name for player 1> ");
+        System.out.print("Enter name for player 1> ");
         String name1 = scanner.nextLine();
-        Player player1 = new Player(name1);
+        players[0] = new Player(name1);
 
-        System.out.println("Enter name for player 2> ");
+        System.out.print("Enter name for player 2> ");
         String name2 = scanner.nextLine();
-        Player player2 = new Player(name2);
+        players[1] = new Player(name2);
 
-        Player currentPlayer = player1;
-        Player nextPlayer = player2;
         boolean won = false;
+        int turn = 0;
 
         while (!won) {
+            Player currentPlayer = players[turn];
             System.out.println("It is "+currentPlayer.getName()+"'s turn.");
             System.out.println("You can enter the following commands:");
             System.out.println("1. \"p\" for punching.");
             System.out.println("2. \"k\" for kicking.");
 
-            doCommand(currentPlayer, nextPlayer);
-            won = checkWin(currentPlayer, nextPlayer);
+            int next = turn + 1;
+            if (next >= players.length) {
+                next = 0;
+            }
+
+            doCommand(currentPlayer, players[next]);
+            won = checkWin(currentPlayer, players[next]);
 
             if (!won) {
-                if (currentPlayer == player1) {
-                    currentPlayer = player2;
-                    nextPlayer = player1;
-                } else {
-                    currentPlayer = player1;
-                    nextPlayer = player2;
-                }
+                turn = 1 - turn;
             }
         }
     }
